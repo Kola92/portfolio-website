2d15fe58848480d960c20a700995bd29a9cdf778
Adekola Olawale <rolawale92@gmail.com>
2021-12-28 21:27:18 +0100

    adjust accomplishments gallery section

--- src/components/Accomplishments/Accomplishments.js
+++ src/components/Accomplishments/Accomplishments.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 import {
   Section,
@@ -7,24 +7,43 @@ import {
 } from "../../styles/GlobalComponents";
 import { Box, Boxes, BoxText, Img } from "./AccomplishmentsStyles";
 import { Achievements } from "../../constants/constants_copy";
+import { IoCloseCircleOutline } from "react-icons/io5";
 
 
 
-const Accomplishments = () => (
-  <Section>
-    <SectionDivider />
-    <SectionTitle>Personal Accomplishments</SectionTitle>
-    <Boxes>
-      {Achievements.map((card, index) => (
-        <Box key={index} style={{ height: "fit-content" }}>
-          <Img src={card.image} />
-          <BoxText style={{ fontSize: "1.3rem", fontWeight: "700" }}>
-            {card.description}
-          </BoxText>
-        </Box>
-      ))}
-    </Boxes>
-  </Section>
-);
+const Accomplishments = () => {
+  const [modal, setModal] = useState(false);
+  const [tempImgSrc, setTempImgSrc] = useState("");
+
+  const getImg = (imgSrc) => {
+    setTempImgSrc(imgSrc);
+    setModal(true);
+  };
+
+  return (
+    <Section>
+      <SectionDivider />
+      <SectionTitle>Personal Accomplishments</SectionTitle>
+      <div className={modal ? "modal open" : "modal"}>
+        <Img src={tempImgSrc} />
+        <IoCloseCircleOutline size={32} onClick={() => setModal(false)} />
+      </div>
+      <Boxes>
+        {Achievements.map((card, index) => (
+          <Box
+            key={index}
+            style={{ height: "fit-content" }}
+            onClick={() => getImg(card.image)}
+          >
+            <Img src={card.image} />
+            <BoxText style={{ fontSize: "1.3rem", fontWeight: "700" }}>
+              {card.description}
+            </BoxText>
+          </Box>
+        ))}
+      </Boxes>
+    </Section>
+  );
+}
 
 export default Accomplishments;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import Theme from '../styles/theme';
+import "../components/Accomplishments/image-modal.css";
 
 export default function App({ Component, pageProps }) {
   return (
