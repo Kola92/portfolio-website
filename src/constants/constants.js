39dd1a650de021279b7a6df0573ff2d67853f15c
Kola92 <rolawale92@gmail.com>
2022-12-11 18:23:15 +0100

    added token prismproject & aws certification

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,53 +1,63 @@
 export const projects = [
   {
+    title: "Token Prism",
+    description:
+    "Token Prism is a simple web application that allows you to track the ever evolving value and news of cryptocurrency tokens in real time. It uses the CoinMarketCap API to get the latest data",
+    image: "/images/projects/token-prism.webp",
+    tags: ["React", "RapidAPI", "Redux Toolkit Query", "Ant Design"],
+    source: "https://github.com/Kola92/token-prism",
+    visit: "https://tokenprism.netlify.app/",
+    id: 0,
+  },
+  {
     title: "Lumina Chat",
     description:
       "Lumina Chat is a chat application that allows users to create chat rooms and chat with other users. It is built using React, Firebase, Context API, and SASS.",
-    image: "/images/lumina-chat.png",
+    image: "/images/projects/lumina-chat.png",
     tags: ["React", "Firestore", "Cloud Storage", "Context API", "SASS"],
     source: "https://github.com/Kola92/lumina-chat",
     visit: "https://lumina-chat.netlify.app",
-    id: 0,
+    id: 1,
   },
   {
     title: "Slick Store Ecommerce",
     description:
       "Slick Store Ecommerce website is a fully functional e-commerce website built using React, Commercejs, Stripe and Material UI. It allows users to add products to cart, checkout and pay for products using Stripe.",
-    image: "/images/slick-store.webp",
+    image: "/images/projects/slick-store.webp",
     tags: ["React", "Commercejs API", "MaterialUI", "Stripe"],
     source: "https://github.com/Kola92/slick-stores-ecommerce",
     visit: "https://slick-store.netlify.app",
-    id: 1,
+    id: 2,
   },
   {
     title: "Soundmagix Song Writers",
     description:
       "Sound Magix SongWriters WordPress is a music-themed website that display an array of songs with different genres(Afrobeat, Afropop, Pop, RnB, Hip Hop etc) and songs can be grouped together in a page according to the navigation menu that is selected.",
-    image: "/images/soundmagix.webp",
+    image: "/images/projects/soundmagix.webp",
     tags: ["WordPress", "Paystack API", "PHP"],
     source: "https://github.com/Kola92",
     visit: "https://soundmagixsongs.com",
-    id: 2,
+    id: 3,
   },
   {
     title: "Phenomenal Furniture Store",
     description:
       "It is an ecommerce web app made with HTML, CSS & Vanilla JavaScript and displaying content entries using the Contentful API.",
-    image: "/images/phenomenal-furnitures.webp",
+    image: "/images/projects/phenomenal-furnitures.webp",
     tags: ["HTML", "CSS", "Vanilla JS", "Contentful API"],
     source: "https://google.com",
     visit: "https://phenomenal-furnitures.netlify.app/",
-    id: 3,
+    id: 4,
   },
   {
     title: "Motion Picture App",
     description:
       "Motion Picture App is a web application built with React and plain CSS for styling that displays all kinds of movies, both classic and modern, greatest and mediocre movies all in one place. All you have to do is search for the movie you want.",
-    image: "/images/motion-pic.webp",
+    image: "/images/projects/motion-pic.webp",
     tags: ["React", "MovieDB API", "CSS"],
     source: "https://github.com/Kola92/movie-app",
     visit: "https://motionpicture-app.netlify.app",
-    id: 4,
+    id: 5,
   },
 ];
 
@@ -58,24 +68,30 @@ export const TimeLineData = [
   { year: 2019, text: "Began learning Reactjs and building web applications" },
   { year: 2020, text: "Participated in Build4SDG Africa  by Facebook's DevC & Andela" },
   { year: 2021, text: "Preparing for Google Cloud Platform & Amazon Web Services cloud developer certification" },
+  { year: 2022, text: "Secured Amazon Web Services Cloud Practitioner certification" },
 ];
 
 export const Achievements = [
   {
-    image: "/images/Andela_Google Front_End_Course.pdf.webp",
+    image: "/images/achievements/AWS_Certified_Cloud_Practitioner_certificate.png",
+    description:
+      "Amazon Web Services Cloud Practitioner Certificate",
+  },
+  {
+    image: "/images/achievements/Andela_Google Front_End_Course.webp",
     description:
       "Andela Google Africa Developer Scholarship Front End Certificate",
   },
   {
-    image: "/images/alc_sdg_certs_fb_cohort1.webp",
+    image: "/images/achievements/alc_sdg_certs_fb_cohort1.webp",
     description: "Build4SDG Challenge 2020 Certificate",
   },
   {
-    image: "/images/google-digital-skills.webp",
+    image: "/images/achievements/google-digital-skills.webp",
     description: "Google Digital Skills for Africa Certificate",
   },
   {
-    image: "/images/DSCLead-Certificate2021.webp",
+    image: "/images/achievements/DSCLead-Certificate2021.webp",
     description: "Google Developer Student Club Lead Certificate",
   },
 ];
